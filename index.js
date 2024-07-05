acb495ad77a4dd3d42886cfbb604234c90258287
JoyMaveke <Mavekejoy@gmail.com>
2024-07-05 05:16:06 +0300

    arrow function

---
+++ index.js
@@ -0,0 +1,17 @@
+{let a =2000;
+let b =100;
+
+const divide = function(a,b){
+ return a/b;
+}}
+let x =2;
+const square = function(x){
+return x*x ;
+}
+
+let a=3;
+let b=4;
+
+const add = function(a,b){
+ return a+b;
+}
